4385373f3bb542f52bb45f9d6db3e3284ef532ba
Kathryn <kdboudreau1@gmail.com>
2018-03-21 17:16:50 -0400

    adding files for multi modules

--- productWeb/src/main/java/com/KDB/product/servlets/DisplayProductDetails.java
+++ productWeb/src/main/java/com/KDB/product/servlets/DisplayProductDetails.java
@@ -1,10 +1,18 @@
 package com.KDB.product.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.KDB.product.bo.ProductBO;
+import com.KDB.product.dto.Product;
 
 /**
  * Servlet implementation class DisplayProductDetails
@@ -14,7 +22,20 @@ public class DisplayProductDetails extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		WebApplicationContext context = WebApplicationContextUtils.getRequiredWebApplicationContext(this.getServletContext());
+		ProductBO bo = (ProductBO) context.getBean("productbo");
+		Product product = bo.findProduct(Integer.parseInt(request.getParameter("id")));
+		
+		
+		PrintWriter out = response.getWriter();
+		out.print("Product Details:");
+		out.print("Product ID:"+product.getId());
+		out.print("Product Name:"+product.getName());
+		out.print("Product Description:"+product.getDescription());
+		out.print("Product Price:"+product.getPrice());
+		
+		
+
 	}
 
 }
